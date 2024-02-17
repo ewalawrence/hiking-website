a0bfa15dea2902479eba4d7ce79e42fa3fe429e6
Your Name <ewalawrence1991@gmail.com>
2024-02-17 03:00:39 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// select header
+const header = document.querySelector('.header');
+// select nav
+const nav = document.querySelector('.nav');
+// select nav trigger
+const navTrigger = document.querySelector('.header__nav-trigger');
+
+// open - close menu
+
+
+// header background on scroll
+
+
+// Initialize Swiper
+
+
+// Scroll Reveal
